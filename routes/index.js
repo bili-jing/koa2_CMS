4955f0f361939640a33666b1682f5ffd6308ce76
LiYan <bili.jing.liyan@gmail.com>
2018-12-04 21:05:21 +0800

    '前台静态页面完成'

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,42 @@
-const 
+const
     Router = require('koa-router');
 
 var router = new Router();
 
 
-router.get('/',async (ctx)=>{
-    ctx.body = '后台管理首页'
+router.get('/', async (ctx) => {
+
+    ctx.render('default/index');
 })
 
+router.get('/news', async (ctx) => {
 
+    ctx.render('default/news');
+
+})
+
+router.get('/service', async (ctx) => {
+
+    ctx.render('default/service');
+
+})
+
+router.get('/about', async (ctx) => {
+
+    ctx.render('default/about');
+
+})
+
+router.get('/case', async (ctx) => {
+
+    ctx.render('default/case');
+
+})
+
+router.get('/connect', async (ctx) => {
+
+    ctx.render('default/connect');
+})
 
 //暴露：
 module.exports = router.routes();

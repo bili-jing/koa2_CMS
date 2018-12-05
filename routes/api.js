c5fae12454b0324ba0e6caaff9c8e4c078068257
LiYan <bili.jing.liyan@gmail.com>
2018-12-05 22:03:32 +0800

    设计api利用cors后台跨域

--- routes/api.js
+++ routes/api.js
@@ -2,13 +2,42 @@ const
     Router = require('koa-router');
 
 var router = new Router();
+var DB = require('../module/db');
 
 
 
 router.get('/',async (ctx)=>{
-    ctx.body = 'api接口'
+    ctx.body="api接口";
 })
 
+
+router.get('/catelist',async (ctx)=>{
+
+    var result=await DB.find('articlecate',{})
+
+    //console.log(result);
+    ctx.body={
+        result:result
+    };
+})
+
+
+router.get('/newslist',async (ctx)=>{
+
+    var page=ctx.query.page || 1;
+
+    var pageSize=5
+
+    var result=await DB.find('article',{},{'_id':1,"title":1},{
+        page,
+        pageSize
+    })
+
+    //console.log(result);
+    ctx.body={
+        result:result
+    };
+})
 
 
 //暴露：
